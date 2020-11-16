c250f7457d8a3bf5227396e2bf1fc37b7d38174a
Bruno Bilheri <52968377+brunoBMonteiro@users.noreply.github.com>
2020-11-16 15:18:41 -0300

    erro do js corrigido

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 const modalOverlay = document.querySelector('.modal-overlay')
-const cards = document.querySelectorAll('card')
+const cards = document.querySelectorAll('.card')
 
 for (let card of cards) {
     card.addEventListener("click", function(){
