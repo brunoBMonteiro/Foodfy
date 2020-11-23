eb3c8fc1b5a66038a5910e77f089cbd6423d34c0
Bruno Bilheri <52968377+brunoBMonteiro@users.noreply.github.com>
2020-11-23 14:23:28 -0300

    problema resolvido no js, e front da aplicação finalizada

--- scripts.js
+++ scripts.js
@@ -4,11 +4,13 @@ const cards = document.querySelectorAll('.card')
 for (let card of cards) {
     card.addEventListener("click", function(){
         const id = card.getAttribute('id')
-        
+        const product = card.querySelector('h2').innerHTML
+        const author = card.querySelector('h3').innerHTML
 
         modalOverlay.classList.add('active')
         modalOverlay.querySelector('img').src = id;
-        
+        modalOverlay.querySelector('h2').innerHTML = product
+        modalOverlay.querySelector('h3').innerHTML = author
     })
 }
 
