31e8c21a16b1bc4e5c9f50c0cd6c491998b1e701
brunoBMonteiro <brunobilheri@hotmail.com>
2021-02-09 15:25:43 -0300

    corrigindo pequenas pequenas coisas

--- scripts.js
+++ scripts.js
@@ -1,16 +1,16 @@
-const modalOverlay = document.querySelector('.modal-overlay')
-const cards = document.querySelectorAll('.card')
+const modalOverlay = document.querySelector(".modal-overlay")
+const cards = document.querySelectorAll(".card")
 
 for (let card of cards) {
     card.addEventListener("click", function(){
-        const id = card.getAttribute('id')
-        const product = card.querySelector('h2').innerHTML
-        const author = card.querySelector('h3').innerHTML
+        const id = card.getAttribute("id")
+        const product = card.querySelector("h2").innerHTML
+        const author = card.querySelector("h3").innerHTML
 
-        modalOverlay.classList.add('active')
-        modalOverlay.querySelector('img').src = id;
-        modalOverlay.querySelector('h2').innerHTML = product
-        modalOverlay.querySelector('h3').innerHTML = author
+        modalOverlay.classList.add("active")
+        modalOverlay.querySelector("img").src = id;
+        modalOverlay.querySelector("h2").innerHTML = product
+        modalOverlay.querySelector("h3").innerHTML = author
     })
 }
 
